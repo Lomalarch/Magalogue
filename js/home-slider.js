09d2636d8bca4a6cfd78f05ee0dbfec20e5c272f
Lomalarch <lomalarch@gmail.com>
2021-02-24 15:40:12 +0100

    no autoplay if user has disabled animations

--- js/home-slider.js
+++ js/home-slider.js
@@ -1,6 +1,18 @@
+/*const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+if (!mediaQuery || mediaQuery.matches) {
+    const animate = false;
+} else {
+    const animate = true;
+}*/
 $(document).ready(function(){
+const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+if (!mediaQuery || mediaQuery.matches) {
+    var animate = false;
+} else {
+    var animate = true;
+}
   $('#topshelf').slick({
-    autoplay: true,
+    autoplay: animate,
     autoplaySpeed: 5000,
     dots: true
   });
