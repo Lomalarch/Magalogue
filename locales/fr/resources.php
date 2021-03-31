13407ad2a9b91cda48e8dd73d8119c758394bfba
Lomalarch <lomalarch@gmail.com>
2021-03-31 11:01:23 +0200

    Help and translation

---
+++ locales/fr/resources.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Dotclear
+ * @subpackage Themes
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+
+if (!isset($__resources['help']['magalogue'])) {
+    $__resources['help']['magalogue'] = dirname(__FILE__) . '/help/help.html';
+}
