38cfc5c1ded998d945aa3fb43f3fba89fa54c2af
Lomalarch <lomalarch@gmail.com>
2026-02-28 22:01:49 +0100

    NEW: version 3.5

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Magalogue',                    // Name
     'Thème adaptatif',              // Description
     'Julien Jakoby / Noé Cendrier', // Author
-    '3.4',                          // Version
+    '3.5',                          // Version
     [                               // Properties
         'requires'    => [['core', '2.37']],
         'standalone_config' => true,
